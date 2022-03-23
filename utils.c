871293005606d1c4aa992a5554120ca0fa7cf304
Sylvain <sylvain.rais24@gmail.com>
2022-03-23 20:07:21 +0100

    utils.c function corrections

--- utils.c
+++ utils.c
@@ -5,31 +5,25 @@
 
 thread_t pop_first(thread_t * fifo){
     thread_t first = *fifo;
-    //printf("first : %p\n", *first);
-    //(*first).next = NULL;
-    printf("fifo -> %p\n", fifo);
-    printf("fifo -> %p\n", *fifo);
     if (*fifo)
         *fifo = (*fifo)->next;
-    else
-        fifo = &(*fifo)->next;
     return first;
 }
 
-void push_last(thread_t* fifo, thread_t last){
+void push_last(thread_t * fifo, thread_t last){
     if (*fifo == NULL) {
         *fifo = last;
         return;
     }
-
-    while ((*fifo)->next){
-        *fifo = (*fifo)->next;
+    thread_t ptr = *fifo;
+    while (ptr->next){
+        ptr = ptr->next;
     }
-    (*fifo)->next = last;
+    ptr->next = last;
 }
 
 // ! les traitements autres que sur la fifo ne sont pas supportés
 void run_next_thread(thread_t * fifo, thread_t * t_running){
-    // push_last(*fifo, *t_running);
+    push_last(fifo, *t_running);
     *t_running = pop_first(fifo);
 }
