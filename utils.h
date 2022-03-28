a972aaa6f7665524a61c426e001a5a7b271081b3
Sylvain Raïs <sylvain.rais24@gmail.com>
2022-03-28 20:09:25 +0200

    adding future useful lock functions

--- utils.h
+++ utils.h
@@ -2,7 +2,12 @@
 #define __UTILS_H__
 
 #include "thread.h"
+int create_lock(void ** lock_table);
 
+void realloc_lock_table(void *** table, int * current_size);
 
+int is_lockable(int lock);
+
+int detect_dead_lock();
 
 #endif // __UTILS_H__
