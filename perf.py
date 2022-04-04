8a6221eb8d48e0597fde31704f0033b98acb3888
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2022-04-04 10:51:29 +0200

    parser python

--- perf.py
+++ perf.py
@@ -4,25 +4,37 @@ import subprocess
 import sys
 import matplotlib.pyplot as plt
 
-
 args = sys.argv[1:]
 
 if (len(args) < 2):
     print("Erreur - Besoin du nom du 1er fichier et le nombre de threads")
     exit()
 
+def parser(result):
+    spliter = result.stdout.decode("utf-8").split(" ")
+    return tuple([int(spliter[-2]), spliter[-1]])
+
 def getTime(fileName):
     if len(args) == 3:
-        subprocess.run(["./" + fileName, str(nb_threads), str(nb_yields)])
+        result = subprocess.run(["./" + fileName, str(nb_threads), str(nb_yields)], stdout = subprocess.PIPE)
     elif len(args) == 2:
-        subprocess.run(["./" + fileName, str(nb_threads)])
+        result = subprocess.run(["./" + fileName, str(nb_threads)], stdout = subprocess.PIPE)
 
+    """
     f = open("time.txt", "r")
     timeString = f.read()
     times = timeString.split("\n")
     f.close()
+    """
 
-    return float(times[0])
+    (time, unit) = parser(result)
+
+    if unit == "us":
+        return time/1000
+    elif unit == "s":
+        return time*1000
+
+    return time
 
 time1 = []
 time2 = []
@@ -35,6 +47,7 @@ if len(args) == 3:
     nb_yields = int(args[2])
 step = 10
 
+
 for i in range(1, nb_threads, step):
     time1.append(getTime(filename1))
     time2.append(getTime(filename2))
@@ -45,7 +58,7 @@ RangeThreads = range(1, nb_threads, step)
 
 plt.plot(RangeThreads, time1, '-r')
 plt.plot(RangeThreads, time2, '-b')
-plt.ylabel("Temps en secondes")
+plt.ylabel("Temps en ms")
 plt.xlabel("Nombre de threads")
 plt.title("Performances de notre librairie et de la librairie pthread")
 plt.show()
