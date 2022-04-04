9b56c9483f8a47678609dcd5eec911e755862fe8
Sylvain Raïs <sylvain.rais24@gmail.com>
2022-04-04 19:33:12 +0200

    removing dead code in utils files, removing dead line in the makefile

--- utils.h
+++ utils.h
@@ -18,19 +18,4 @@ int get_mutex_ind();
 // Resize the allocated array of thread_mutex_t pointers (return 0 if request failed)
 void realloc_mutex_table(thread_mutex_t *** table, size_t * current_size);
 
-// // Create a mutex_chain element
-// struct mutex_chain * alloc_mutex_element(int mutex_index, struct mutex_chain * next);
-
-// // Free the given mutex chain
-// void free_mutex_chain(struct mutex_chain * begin);
-
-// // Analyse if the given mutex index belongs to the mutex chain
-// int belong_to(int mut_ind, struct mutex_chain * begin);
-
-// // Assert if a dead lock occured
-// int detect_dead_lock_from_begining(thread_mutex_t ** table, int begin_lock);
-
-// // Give the first process to execute to come back on a lock chain
-// thread_t get_last_mutex_queue(thread_mutex_t ** table, thread_t t);
-
 #endif // __UTILS_H__
