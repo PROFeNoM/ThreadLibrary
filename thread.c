0c0bc4ebc5d2ecfeb43c3b7067328f6d404b4555
Alexandre Choura <alexandre.choura@bordeaux-inp.fr>
2022-04-20 13:28:50 +0200

    Remove the sleepq

--- thread.c
+++ thread.c
@@ -13,7 +13,6 @@
 #endif
 
 TAILQ_HEAD(, thread_struct) runq;
-TAILQ_HEAD(, thread_struct) sleepq;
 TAILQ_HEAD(, thread_struct) lockq;
 thread_t T_RUNNING = NULL;
 thread_t T_MAIN = NULL;
@@ -55,8 +54,6 @@ void initialize_thread(thread_t thread, int is_main_thread)
 	initialize_context(thread, !is_main_thread);
 	thread->status = READY;
 	thread->previous_thread = NULL;
-    thread->is_in_sleepq = 0;
-    thread->is_in_lockq = 0;
 }
 
 void free_thread(thread_t thread_to_free)
@@ -75,7 +72,6 @@ void free_thread(thread_t thread_to_free)
 __attribute__((constructor)) void initialize_runq()
 {
 	TAILQ_INIT(&runq);
-	TAILQ_INIT(&sleepq);
 	TAILQ_INIT(&lockq);
 
 	thread_t main_thread = malloc(sizeof(struct thread_struct));
@@ -128,8 +124,6 @@ thread_t get_next_thread()
     {
         // Run waiting thread next
 		thread_t previous_thread = current_thread->previous_thread;
-        previous_thread->is_in_sleepq = 0;
-        TAILQ_REMOVE(&sleepq, previous_thread, next_sleepq);
         return previous_thread;
     }
 
@@ -209,8 +203,6 @@ int thread_join(thread_t thread, void** retval)
 	if (to_wait->status != TERMINATED)
 	{
 		waiting_thread->status = WAITING;
-        waiting_thread->is_in_sleepq = 1;
-		TAILQ_INSERT_TAIL(&sleepq, waiting_thread, next_sleepq);
 		// Current thread waits for thread to wait to terminate
 		while (to_wait->status != TERMINATED)
 		{
@@ -289,7 +281,6 @@ int thread_mutex_lock(thread_mutex_t* mutex)
 
     // If the mutex is locked, the current thread goes to sleep
 	current_thread->status = LOCKED;
-    current_thread->is_in_lockq = 1;  // TODO: To remove
 	TAILQ_INSERT_TAIL(&lockq, current_thread, next_lockq);
 
     // Current thread waits for mutex to be unlocked
@@ -307,7 +298,6 @@ int thread_mutex_unlock(thread_mutex_t* mutex)
     {
         thread_t waiting_thread = TAILQ_FIRST(&lockq);
         waiting_thread->status = RUNNING;
-        waiting_thread->is_in_lockq = 0;
         TAILQ_REMOVE(&lockq, waiting_thread, next_lockq);
     }
 
