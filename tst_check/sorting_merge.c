c756a6067d75760a70b9b5d38923366f1b470d71
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2022-05-06 14:44:03 +0200

    tri fusion without threads working

--- tst_check/sorting_merge.c
+++ tst_check/sorting_merge.c
@@ -10,15 +10,26 @@
 /*
   This test consists in a sorting merge of a large array.
 */
-
 int * sort_two(int * array)
 {
-    if (array[0] > array[1])
+    int length = array[0];
+
+    if (length%2 == 1)
     {
-        int higher = array[0];
-        array[0] = array[1];
-        array[1] = higher;
+        printf("value: %d\n", array[1]);
+        return array;
     }
+    else
+    {
+        if (array[1] > array[2])
+        {
+            int higher = array[1];
+            array[1] = array[2];
+            array[2] = higher;
+        }
+    }
+
+    printf("Array: %d %d\n", array[1], array[2]);
 
     return array;
 }
@@ -26,11 +37,11 @@ int * sort_two(int * array)
 void * divide_or_not(void * a) {
     int * array = (int *) a;
     int length_array = array[0];
+    printf("Length array: %d\n", length_array);
 
     if (length_array > 2)
     {
         int err;
-        //thread_t * newthread_1, * newthread_2;
         thread_t tid[2];
 
         int odd_number = 0;
@@ -38,29 +49,29 @@ void * divide_or_not(void * a) {
             odd_number = 1;
 
 
-        int new_length = length_array / 2;
-        int array_1_with_length[new_length + 1];
-        int array_2_with_length[new_length + 1 + odd_number];
-        void * res_1 = malloc(sizeof(int) * new_length);
-        void * res_2 = malloc(sizeof(int) * new_length);
+        int new_length_values = length_array / 2;
+        int new_length_size = new_length_values + 1;
+
+        int array_1_with_length[new_length_size];
+        int array_2_with_length[new_length_size + odd_number];
+        void * res_1 = malloc(sizeof(int) * new_length_size);
+        void * res_2 = malloc(sizeof(int) * new_length_size);
         int i, k = 1;
 
         // First array
-        for (i = 1; i < new_length + 1; ++i) {
+        for (i = 1; i < new_length_size; ++i) {
             array_1_with_length[i] = array[i];
         }
 
         // Second array
         k = i;
-        for (int j = 1; j < new_length + 1 + odd_number; ++j) {
+        for (int j = 1; j < new_length_size + odd_number; ++j) {
             array_2_with_length[j] = array[k];
             ++k;
         }
 
-        array_1_with_length[0] = new_length;
-        array_2_with_length[0] = new_length + odd_number;
-
-        printf("length: %d\n", array_1_with_length[0]);
+        array_1_with_length[0] = new_length_values;
+        array_2_with_length[0] = new_length_values + odd_number;
 
         err = thread_create(&tid[0], divide_or_not, array_1_with_length);
         assert(!err);
@@ -78,22 +89,65 @@ void * divide_or_not(void * a) {
 
 void sorting_merge(int * array)
 {
-    int number_threads = LENGTH / 2;
+    /*
+    int size_array = LENGTH / 2;
     int* array_two = malloc(sizeof(int) * 2);
 
-    for (int i = 0; i < number_threads; ++i)
-    {
         array_two = divide_or_not(array);
-        printf("0 = %d, 1 = %d\n", array_two[0], array_two[1]);
+        // nhnprintf("0 = %d, 1 = %d\n", array_two[0], array_two[1]);
+    */
+}
+
+
+void triFusion(int i, int j, int tab[], int tmp[]) {
+    if(j <= i){ return;}
+
+    int m = (i + j) / 2;
+
+    triFusion(i, m, tab, tmp);     //trier la moitié gauche récursivement
+    triFusion(m + 1, j, tab, tmp); //trier la moitié droite récursivement
+
+    int pg = i;
+    int pd = m + 1;
+    int c;
+
+
+    for(c = i; c <= j; c++)
+    {
+        if(pg == m + 1) { //le pointeur du sous-tableau de gauche a atteint la limite
+            tmp[c] = tab[pd];
+            pd++;
+        }
+        else if (pd == j + 1) { //le pointeur du sous-tableau de droite a atteint la limite
+            tmp[c] = tab[pg];
+            pg++;
+        }
+        else if (tab[pg] < tab[pd]) { //le pointeur du sous-tableau de gauche pointe vers un élément plus petit
+            tmp[c] = tab[pg];
+            pg++;
+        }
+        else {  //le pointeur du sous-tableau de droite pointe vers un élément plus petit
+            tmp[c] = tab[pd];
+            pd++;
+        }
+    }
+
+    for(c = i; c <= j; c++) {  //copier les éléments de tmp[] à tab[]
+        tab[c] = tmp[c];
     }
 }
 
+
 int main() {
 
-    int array[] = {LENGTH, 1, 3, 5, 4, 9, 6, 5, 2, 1, 8};
-    sorting_merge(array);
+    int array[] = {4, 3, 1, 2};
+    int tmp[1000000];
+    int length = sizeof(array) / sizeof(int);
+    printf("Length array: %d\n", length);
 
-    for(int i=1; i<10; i++)
+    triFusion(0, length, array, tmp);
+
+    for(int i=1; i<length+1; i++)
     {
         printf("value:%d\n", array[i]);
     }
