a1e5c475b5477c364c63c6893665477fe5e51d93
Sylvain Raïs <sylvain.rais24@gmail.com>
2022-05-10 12:00:41 +0200

    rectifying compilation errors

--- tst_check/stack_oveflow.c
+++ tst_check/stack_oveflow.c
@@ -15,15 +15,18 @@ struct heavy
 void * lets_go(void *)
 {
     struct heavy heavy_struct;
-    lets_go(NULL);
+    lets_go(&heavy_struct);
+    return NULL;
 }
 
 int main(int argc, char ** argv) // Executer la fonction récursive dans un thread particulier
 {
     thread_t t;
     void * res;
-    int err = thread_create(t, lets_go, NULL);
+    int err = thread_create(&t, lets_go, NULL);
     assert(!err);
     err = thread_join(t, &res);
     assert(!err);
+
+    return 0;
 }
