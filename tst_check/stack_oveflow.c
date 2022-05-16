33934a97ac56eff1c4809d3b1f34ed507fa4cf47
Sylvain Raïs <sylvain.rais24@gmail.com>
2022-05-16 08:31:18 +0200

    adding printf in stack_overflow

--- tst_check/stack_oveflow.c
+++ tst_check/stack_oveflow.c
@@ -12,8 +12,9 @@ struct heavy
     long long int Sylvain;
 };
 
-void * lets_go(void *)
+void * lets_go(void * p)
 {
+    printf("Pointer : %p", p);
     struct heavy heavy_struct;
     lets_go(&heavy_struct);
     return NULL;
