bad00c6f8129f01e04350e8cd02f83dcb4a207c2
Mathilde POMMIER <mpommier005@enseirb-matmeca.fr>
2022-05-27 17:15:33 +0200

    Cleanning a little the Makefile + transfert arguments management to perf.py

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ DST_TEST_TEST = install/test
 DST_TEST_LIB = install/lib
 TEST_CHECK_DIR = tst_check
 TEST_DIR = tst
+
 TSTFILESO = $(TSTFILES:%=%.o)
 TSTFILES = 	$(DST_TEST_BIN)/01-main \
 						$(DST_TEST_BIN)/02-switch \
@@ -27,16 +28,21 @@ TSTFILES = 	$(DST_TEST_BIN)/01-main \
 						$(DST_TEST_BIN)/71-preemption \
 						$(DST_TEST_BIN)/81-deadlock
 LIBTHREAD = $(DST_TEST_LIB)/libthread.so
-THREADONLY =	$(DST_TEST_BIN)/21-create-many \
-							$(DST_TEST_BIN)/22-create-many-recursive \
-							$(DST_TEST_BIN)/23-create-many-once \
-							$(DST_TEST_BIN)/61-mutex \
-							$(DST_TEST_BIN)/62-mutex
-THREADANDYIELD =	$(DST_TEST_BIN)/31-switch-many \
-									$(DST_TEST_BIN)/32-switch-many-join \
-									$(DST_TEST_BIN)/33-switch-many-cascade
+
+
+FILESGRAPHS = 	$(DST_TEST_BIN)/21-create-many \
+				$(DST_TEST_BIN)/22-create-many-recursive \
+				$(DST_TEST_BIN)/23-create-many-once \
+				$(DST_TEST_BIN)/61-mutex \
+				$(DST_TEST_BIN)/62-mutex \
+				$(DST_TEST_BIN)/31-switch-many \
+				$(DST_TEST_BIN)/32-switch-many-join \
+				$(DST_TEST_BIN)/33-switch-many-cascade
+
 LDLIBRARYPATH = ./install/lib/
 LIBTHREADNAME = thread
+
+# Grouping files according to their number of arguments to run them :
 TSTFILESWITHOUTARGS = $(DST_TEST_BIN)/01-main \
 											$(DST_TEST_BIN)/02-switch \
 											$(DST_TEST_BIN)/03-equity \
@@ -51,12 +57,14 @@ TSTFILESWITHARGS2 =	$(DST_TEST_BIN)/31-switch-many \
 										$(DST_TEST_BIN)/32-switch-many-join
 TSTFILESWITHARGS3 =	$(DST_TEST_BIN)/33-switch-many-cascade
 TSTFILESWITHARGS4 =	$(DST_TEST_BIN)/51-fibonacci
+#############################################################################
 
+# Our tests :
 TSTFILESOCHECK = $(TSTFILESCHECK:%=%.o)
 TSTFILESCHECK = $(DST_TEST_TEST)/test_sorting_merge \
 				$(DST_TEST_TEST)/test_sum
+#############################################################################
 
-# TSTFILESCHECK = $(DST_TEST_TEST)/stack_oveflow
 NICE20 = nice -20
 
 
@@ -84,14 +92,17 @@ ifeq ($(USEPTHREAD),0)
 	$(CC) -fPIC $(CFLAGS) $^ -c -o $@
 endif
 
+# Our library
 $(LIBTHREAD): thread.o utils.o
 ifeq ($(USEPTHREAD),0)
 	$(CC) -shared -o $@ $^
 endif
 
+# Creates install/lib, install/bin and install/test directories :
 repositories:
 	mkdir -p install/lib install/bin install/test
 
+# Creates all .o in install/bin/ :
 $(DST_TEST_BIN)/%.o: $(TEST_DIR)/%.c
 ifeq ($(USEPTHREAD),1)
 	$(CC) $(CFLAGS) -c $^ -o $@ $(CBIBFLAG)
@@ -99,6 +110,7 @@ else
 	$(CC) $(CFLAGS) -c $^ -o $@
 endif
 
+# Creates all executables in install/bin/ :
 $(DST_TEST_BIN)/%:
 ifeq ($(USEPTHREAD),1)
 	$(CC) -o $@_c $@.o $(PTHREAD)
@@ -106,14 +118,18 @@ else
 	$(CC) $@.o -L$(LDLIBRARYPATH) -o $@ -l$(LIBTHREADNAME)
 endif
 
-
+# Sub rule of check :
 test_check: repositories $(TSTFILESOCHECK) $(LIBTHREAD) $(TSTFILESCHECK) test_check_exec delete_o_test
-
+# Sub rule of check :
 test_check_pthreads:
 	make -B USEPTHREAD=1 our_test_check_pthreads
 
+## Our tests (with our library and C library) :
 our_test_check_pthreads: repositories $(TSTFILESOCHECK) $(TSTFILESCHECK) test_check_exec_c delete_o_test
 
+our_pthreads: repositories $(TSTFILESO) $(TSTFILES) delete_o_bin
+
+# Creates all .o in install/bin/ :
 $(DST_TEST_TEST)/%.o: $(TEST_CHECK_DIR)/%.c
 ifeq ($(USEPTHREAD),1)
 	$(CC) $(CFLAGS) -c $^ -o $@ $(CBIBFLAG)
@@ -128,6 +144,7 @@ else
 	$(CC) $@.o -L$(LDLIBRARYPATH) -o $@ -l$(LIBTHREADNAME)
 endif
 
+# Runs our tests :
 test_check_exec:
 	for file in $(TSTFILESCHECK) ; do \
 		if [ $$file = "$(DST_TEST_TEST)/test_sum" ] ; then \
@@ -135,9 +152,6 @@ test_check_exec:
 		fi ; \
 		if [ $$file = "$(DST_TEST_TEST)/test_sorting_merge" ] ; then \
 			LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) ./$$file 15 ; \
-		fi ; \
-		if [ $$file = "$(DST_TEST_TEST)/stack_oveflow" ] ; then \
-			LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) ./$$file ; \
 		fi ; \
 	done
 
@@ -151,26 +165,19 @@ test_check_exec_c:
 		fi ; \
 	done
 
+#############################################################################
 
-
-
-
-our_pthreads: repositories $(TSTFILESO) $(TSTFILES) delete_o_bin
-
-
-
+# Creates install/graphs directory :
 repo_graph:
 	mkdir -p install/graphs
 
+# Saves the graphs for the given tests :
 save_graphs:
-	for file_1 in $(THREADONLY) ; do \
-		LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) taskset -c 0 python3 perf.py $$file_1 1000000 ; \
-	done
-	for file_2 in $(THREADANDYIELD) ; do \
-		LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) taskset -c 0 python3 perf.py $$file_2 1000000 10 ; \
+	for file in $(FILESGRAPHS) ; do \
+		LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) taskset -c 0 python3 perf.py $$file ; \
 	done
 
-
+# Saves the graphs for our tests :
 save_graphs_check:
 	for file in $(TSTFILESCHECK) ; do \
 		if [ $$file = "$(DST_TEST_TEST)/test_sum" ] ; then \
@@ -193,6 +200,7 @@ delete_o_test:
 	rm -f *.o
 
 
+# Runs given tests
 exec:
 	for file_1 in $(TSTFILESWITHOUTARGS) ; do \
 		LD_LIBRARY_PATH=$(LDLIBRARYPATH) $(NICE20) ./$$file_1 ; \
--- perf.py
+++ perf.py
@@ -3,12 +3,12 @@ import subprocess
 import sys
 import matplotlib.pyplot as plt
 import re
-# import progress.bar as Bar
+import progress.bar as Bar
 
 args = sys.argv[1:]
 
-if (len(args) < 2):
-    print("Erreur - Besoin du nom du 1er fichier et le nombre de threads")
+if (len(args) < 1):
+    print("Erreur - Besoin du nom du 1er fichier")
     exit()
 
 def parser(result):
@@ -21,10 +21,15 @@ def parser(result):
     return (0, "s")
 
 def getTime(fileName, threads_used):
-    if len(args) == 3:
+    onlyName = fileName.split("/")[2]
+    if ((onlyName == "31-switch-many") or (onlyName == "32-switch-many-join") or (onlyName == "33-switch-many-cascade")):
         result = subprocess.run(["./" + fileName, str(threads_used), str(nb_yields)], stdout = subprocess.PIPE)
-    elif len(args) == 2:
+    else:
         result = subprocess.run(["./" + fileName, str(threads_used)], stdout = subprocess.PIPE)
+    # if len(args) == 3:
+    #     result = subprocess.run(["./" + fileName, str(threads_used), str(nb_yields)], stdout = subprocess.PIPE)
+    # elif len(args) == 2:
+    #     result = subprocess.run(["./" + fileName, str(threads_used)], stdout = subprocess.PIPE)
 
 
     (time, unit) = parser(result)
@@ -42,33 +47,80 @@ time2 = []
 filename1 = args[0]
 filename2 = filename1 + "_c"
 
-nb_threads = int(args[1])
-if nb_threads < 100:
-    print("Cannot have less than 100 threads")
-    raise ValueError()
+#######################################################################################
+# nb_threads = int(args[1])
 
-if len(args) == 3:
-    nb_yields = int(args[2])
-step = int(nb_threads/1000)
+# if len(args) == 3:
+#     nb_yields = int(args[2])
 
-# bar = Bar.Bar('Running ' + filename1 + ' vs. ' + filename2, step=step, max=nb_threads / step, suffix='%(percent).1f%% - %(eta)ds')
+# if ((filename1 == "install/bin/61-mutex") or (filename1 == "install/bin/62-mutex")):
+#     step = nb_threads
+# elif (filename1 == "install/bin/33-switch-many-cascade"):
+#     print("HERE install/bin/33-switch-many-cascade HERE")
+#     nb_threads = 20
+#     step = 1
+# else:
+#     step = int(nb_threads/100)
+#######################################################################################
+
+name = filename1.split("/")[2]
+if ((name == "21-create-many") or (name == "22-create-many-recursive") or (name == "23-create-many-once")):
+    nb_threads = 10000
+    step = int(nb_threads/100)
+elif ((name == "61-mutex") or (name == "62-mutex")):
+    nb_threads = 20
+    step = 2
+elif ((name == "31-switch-many") or (name == "32-switch-many-join")):
+    nb_threads = 10000
+    nb_yields = 10
+    step = int(nb_threads/100)
+elif (name == "33-switch-many-cascade"):
+    nb_threads = 1000
+    nb_yields = 10
+    step = int(nb_threads/100)
+else:
+    nb_threads = 10000
+    step = int(nb_threads/100)
+
+
+bar = Bar.Bar('Running ' + filename1.split("/")[2] + ' vs. ' + filename2.split("/")[2], step=step, max=nb_threads / step, suffix='%(percent).1f%% - %(eta)ds' + ' ')
 for i in range(1, nb_threads, step):
-    #print(f'Running {filename1} & {filename2} ==> {i}.' + '\r')
-
+    print(f'Running {filename1.split("/")[2]} & {filename2.split("/")[2]} ==> {i}.' + '\r')
+    
     time1.append(getTime(filename1, i))
     time2.append(getTime(filename2, i))
-    # bar.next()
-# bar.finish()
+    bar.next()
+bar.finish()
 
 RangeThreads = range(1, nb_threads, step)
+
+titles = {
+    "install/bin/21-create-many" : "Performances de notre bibliothèque et de la bibliothèque pthread\n21-create-many",
+    "install/bin/22-create-many-recursive" : "Performances de notre bibliothèque et de la bibliothèque pthread\n22-create-many-recursive",
+    "install/bin/23-create-many-once" : "Performances de notre bibliothèque et de la bibliothèque pthread\n23-create-many-once",
+    "install/bin/61-mutex" : "Performances de notre bibliothèque et de la bibliothèque pthread\n61-mutex",
+    "install/bin/62-mutex" : "Performances de notre bibliothèque et de la bibliothèque pthread\n62-mutex",
+    "install/bin/31-switch-many" : "Performances de notre bibliothèque et de la bibliothèque pthread\n31-switch-many",
+    "install/bin/32-switch-many-join" : "Performances de notre bibliothèque et de la bibliothèque pthread\n32-switch-many-join",
+    "install/bin/33-switch-many-cascade" : "Performances de notre bibliothèque et de la bibliothèque pthread\n33-switch-many-cascade"
+}
+
+final_time1 = time1[len(time1) - 1]
+final_time2 = time2[len(time2) - 1]
+unit = "us"
+
+if final_time2 > 100000:
+    final_time2 = final_time2 / 1000
+    unit = "ms"
+    time1 = [time / 1000 for time in time1]
+    time2 = [time / 1000 for time in time2]
 
 
 plt.plot(RangeThreads, time1, '-r', label='notre bib')
 plt.plot(RangeThreads, time2, '-b', label='pthread bib')
-plt.ylabel("Temps en ms")
+plt.ylabel("Temps en " + unit)
 plt.xlabel("Nombre de threads")
-plt.title("Performances de notre bibliothèque et de la bibliothèque pthread")
+plt.title(titles[filename1])
 plt.legend()
 # plt.show()
-name = filename1.split("/")
-plt.savefig('install/graphs/' + name[2])
+plt.savefig('install/graphs/' + name)
